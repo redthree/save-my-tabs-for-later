0bcc67c84a5489d22da23c6c4e5c74556a60c40d
Gonzalo Castillo <g.c.cifuentes@gmail.com>
2018-09-04 18:54:35 -0300

    First commit

---
+++ background.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var respMessage = document.getElementById('respMessage');
+	
+	chrome.storage.sync.get(['tabsArr'], function(result){
+		
+		if(result.tabsArr !== undefined){
+
+			respMessage.innerHTML = '<button id="restoreTabs">Restore tabs</button><br/><button id="clearTabs">Clear Tabs</button>';
+			
+			var btnRestoreTabs = document.getElementById('restoreTabs');
+			var btnClearTabs = document.getElementById('clearTabs');
+			
+			btnRestoreTabs.addEventListener('click', function(){
+				result.tabsArr.forEach(function(tab){
+					chrome.tabs.create({
+						url: tab.url
+					});
+				});
+			});
+
+			btnClearTabs.addEventListener('click', function(){
+				chrome.storage.sync.remove('tabsArr', function(){
+					respMessage.innerHTML = 'Tabs cleared';
+				});
+			});
+
+		} else {
+			respMessage.innerHTML = '<button id="saveTabs">Save Tabs</button>';
+
+			var btnSaveTabs = document.getElementById('saveTabs');
+
+			btnSaveTabs.addEventListener('click', function(){
+				chrome.tabs.query({currentWindow: true}, function(tabs){
+					chrome.storage.sync.clear(null);
+					chrome.storage.sync.set({'tabsArr': tabs}, function(){
+						respMessage.innerHTML = 'Tabs are saved';
+					});
+				});
+			});
+		}
+	});
+	
+});
